af8bbda15be7732e2438cef7e7b2596a56652f2c
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-12-21 15:04:31 +0300

    Listview Yapısı ile kişiler anasayfada görüntülendi

--- RehberApp/rehberapp/lib/ui/views/ana_sayfa.dart
+++ RehberApp/rehberapp/lib/ui/views/ana_sayfa.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rehberapp/data/entity/kisiler.dart';
+import 'package:rehberapp/ui/views/detay_sayfa.dart';
 import 'package:rehberapp/ui/views/kayit_sayfa.dart';
 
 class AnaSayfa extends StatefulWidget {
@@ -9,12 +11,94 @@ class AnaSayfa extends StatefulWidget {
 }
 
 class _AnaSayfaState extends State<AnaSayfa> {
+  bool aramaYapiliyormu = false;
+
+  void ara(String aramaKelimesi){
+    print("Kisi ara = $aramaKelimesi");
+  }
+
+ Future<List<Kisiler>> KisileriYukle() async {
+  var KisilerListesi = <Kisiler>[];
+  var k1 = Kisiler(kisi_id: 1, kisi_ad: "Ahmet", kisi_tel: "111111");
+  var k2 = Kisiler(kisi_id: 2, kisi_ad: "Halil", kisi_tel: "222222");
+  var k3 = Kisiler(kisi_id: 3, kisi_ad: "Duygu", kisi_tel: "333333");
+  
+  KisilerListesi.add(k1);
+  KisilerListesi.add(k2);
+  KisilerListesi.add(k3);
+
+  return KisilerListesi;
+
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Kisiler"),),
+      appBar: AppBar(title: aramaYapiliyormu ? 
+      TextField(decoration: const InputDecoration(hintText: "Ara"),
+      onChanged: (aramaSonucu){
+        ara(aramaSonucu);
+      },
+      
+      ) :
+      const Text("Kisiler"),
+      
+      actions: [
+        aramaYapiliyormu ? 
+        IconButton(onPressed: (){
+          setState(() {
+            aramaYapiliyormu = false;
+          });
+        }, icon:const Icon(Icons.clear)) : 
 
-      body: Center(),
+        IconButton(onPressed: (){
+          setState(() {
+            aramaYapiliyormu = true;
+          });         
+        }, icon: const Icon(Icons.search))
+
+      ],
+      ),
+
+      body:FutureBuilder<List<Kisiler>>(future: KisileriYukle(),
+       builder: (context,snapshot ){
+        if(snapshot.hasData){
+          var kisilerListesi = snapshot.data;
+          return ListView.builder(
+            itemCount: kisilerListesi!.length,
+            itemBuilder: (context,indeks){
+              var kisi = kisilerListesi[indeks];
+              return Card(
+                child: Row(
+                  children: [
+                    SizedBox(height: 100,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(kisi.kisi_ad,style: TextStyle(fontSize: 20),),
+                            Text(kisi.kisi_tel)
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(onPressed: (){
+                      
+                    }, icon: const Icon(Icons.clear,color: Colors.black54,))
+                  ],
+                ),
+              );
+            },
+          );
+
+
+        }else{
+          return const Center(); //Eğer veri yoksa bos bir sayfa goster
+        }
+       },
+       ),
        floatingActionButton: FloatingActionButton(onPressed: (){
         Navigator.push(context, MaterialPageRoute(builder: (context)=>const KayitSayfa()))
         .then((value){
--- RehberApp/rehberapp/lib/ui/views/detay_sayfa.dart
+++ RehberApp/rehberapp/lib/ui/views/detay_sayfa.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:rehberapp/data/entity/kisiler.dart';
 
 class DetaySayfa extends StatefulWidget {
-  const DetaySayfa({super.key});
+
+  Kisiler kisi;
+  DetaySayfa({required this.kisi});
 
   @override
   State<DetaySayfa> createState() => _DetaySayfaState();
 }
 
 class _DetaySayfaState extends State<DetaySayfa> {
+
+  var tfKisiAdi = TextEditingController();
+  var tfKisiTel = TextEditingController();
+
+  Future<void> Guncelle(int kisi_id,String kisi_ad, String kisi_tel) async {
+    print("guncelle : $kisi_id, $kisi_ad, $kisi_tel");
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var kisi = widget.kisi;
+    tfKisiAdi.text = kisi.kisi_ad;
+    tfKisiTel.text = kisi.kisi_tel;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Detay Sayfasi"),),
 
-      body: Center()
+      body: Center(
+        child: Padding(
+         padding: const EdgeInsets.only(left: 50,right: 50),
+         child: Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(controller: tfKisiAdi,decoration: const InputDecoration(hintText: "Kisi ad"),),
+            TextField(controller:tfKisiTel,decoration: const InputDecoration(hintText: "Kisi Tel"),),
+
+            ElevatedButton(onPressed: (){
+              Guncelle(widget.kisi.kisi_id, tfKisiAdi.text, tfKisiTel.text);
+            }, child: const Text("Guncelle"))
+          ],
+         ),
+        ),
+      ),
     );
   }
 }
